8f8fe1d02c924a336b968ddc92f196a7179f9862
Thitipong Roongprasert <thitipong@computerlogy.com>
2022-11-27 22:05:20 +0700

    remove lib unuse

--- src/theme.js
+++ src/theme.js
@@ -1,6 +1,5 @@
 import { createContext, useState, useMemo } from "react";
 import { createTheme  } from "@mui/material/styles";
-import { color } from "@mui/system";
 
 // color design tokens
 export const tokens = mode => ({
